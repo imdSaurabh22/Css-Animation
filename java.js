10db0b811f9730e75e808f95cc9ea23c7a46c9fa
Saurabh Chauhan <149091345+imdSaurabh22@users.noreply.github.com>
2024-08-14 01:40:13 -0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,24 @@
+const singleHello = document.getElementById('single-hello');
+
+// Function to generate a random color
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Change text color randomly every 2 sec
+function changeTextColor() {
+    singleHello.style.color = getRandomColor();
+}
+
+
+
+// Change text color every 2 sec (2000 milliseconds)
+setInterval(changeTextColor, 2000);
+
+// Initial color change
+changeTextColor();
